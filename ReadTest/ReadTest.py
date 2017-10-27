6660fa6970e5dcfadc77de344a6099b66aae3fb1
zhaochaojian <zhaochaojian@nibirutech.com>
2017-10-27 11:14:43 +0800

    first commit

---
+++ ReadTest/ReadTest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#coding=utf-8
+if __name__ == '__main__':
+    #标准输入，input读取合法的python表达式
+    n=input()
+    n1=input()
+
+    #标准格式化输出
+    print("n=%d, n1=%d"%(n,n1))
+    #raw_input读取字符串
+    s=raw_input()
+    print(s)
